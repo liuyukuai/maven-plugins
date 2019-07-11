3172fa42b771756ceaf916b2e3b1026e99afab26
liuyk <271007729@qq.com>
2019-07-11 23:51:04 +0800

    修改注释生成方法

---
+++ maven-plugins/itxiaoer-mybatis-generator-plugin/src/main/java/com/itxiaoer/plugins/mybatis/CommentGenerator.java
@@ -0,0 +1,33 @@
+package com.itxiaoer.plugins.mybatis;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.InnerClass;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+/**
+ * @author : liuyk
+ */
+@SuppressWarnings("unused")
+public class CommentGenerator extends DefaultCommentGenerator {
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        field.addJavaDocLine("/**");
+        String remarks = introspectedColumn.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            int var7 = remarkLines.length;
+
+            for (int var8 = 0; var8 < var7; ++var8) {
+                String remarkLine = remarkLines[var8];
+                field.addJavaDocLine(" *   " + remarkLine);
+            }
+        }
+
+        field.addJavaDocLine(" *");
+        field.addJavaDocLine(" */");
+    }
+}
--- maven-plugins/itxiaoer-mybatis-generator-plugin/src/main/java/com/itxiaoer/plugins/mybatis/DomainExtendPlugin.java
+++ maven-plugins/itxiaoer-mybatis-generator-plugin/src/main/java/com/itxiaoer/plugins/mybatis/DomainExtendPlugin.java
@@ -1,11 +1,13 @@
 package com.itxiaoer.plugins.mybatis;
 
 import com.itxiaoer.plugins.mybatis.util.JavaDocUtils;
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * mybatis 代码生成扩展（javadoc）
@@ -41,6 +43,43 @@ public class DomainExtendPlugin extends PluginAdapter {
             innerEnum.addAnnotation("@SuppressWarnings(\"ALL\")");
         }
 
+
+
+        String author = this.getProperties().getProperty("author");
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine("* " + introspectedTable.getRemarks());
+        topLevelClass.addJavaDocLine("* ");
+        topLevelClass.addJavaDocLine("* @author : " + (Objects.isNull(author) ? "liuyk" : author));
+        topLevelClass.addJavaDocLine("*/");
+
         return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
     }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+
+        String classAnnotation = "@ApiModel(value=\"" + introspectedTable.getRemarks() + "\")";
+        if (!topLevelClass.getAnnotations().contains(classAnnotation)) {
+            topLevelClass.addAnnotation(classAnnotation);
+        }
+        String apiModelAnnotationPackage = this.properties.getProperty("apiModelAnnotationPackage");
+        String apiModelPropertyAnnotationPackage = this.properties.getProperty("apiModelPropertyAnnotationPackage");
+        if (Objects.isNull(apiModelAnnotationPackage) || apiModelPropertyAnnotationPackage.isEmpty()) {
+            apiModelAnnotationPackage = "io.swagger.annotations.ApiModel";
+        }
+        if (Objects.isNull(apiModelPropertyAnnotationPackage) || apiModelPropertyAnnotationPackage.isEmpty()) {
+            apiModelPropertyAnnotationPackage = "io.swagger.annotations.ApiModelProperty";
+        }
+        topLevelClass.addImportedType(apiModelAnnotationPackage);
+        topLevelClass.addImportedType(apiModelPropertyAnnotationPackage);
+        field.addAnnotation("@ApiModelProperty(value=\"" + introspectedColumn.getRemarks() +
+                "\",name=\"" + introspectedColumn.getJavaProperty() +
+                "\",dataType=\"" + introspectedColumn.getFullyQualifiedJavaType().getShortName() +
+                "\")");
+
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+
+
 }
