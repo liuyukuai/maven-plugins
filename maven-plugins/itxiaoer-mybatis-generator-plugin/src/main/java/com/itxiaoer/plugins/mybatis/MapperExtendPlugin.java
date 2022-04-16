c19c38305d0e08ce2e42419b5e93c4eaa482b4cd
liuyk <liuyk@tsingyun.net>
2022-04-16 17:22:21 +0800

    增加ignore方法

--- maven-plugins/itxiaoer-mybatis-generator-plugin/src/main/java/com/itxiaoer/plugins/mybatis/MapperExtendPlugin.java
+++ maven-plugins/itxiaoer-mybatis-generator-plugin/src/main/java/com/itxiaoer/plugins/mybatis/MapperExtendPlugin.java
@@ -3,6 +3,7 @@ package com.itxiaoer.plugins.mybatis;
 import com.itxiaoer.commons.core.util.Lists;
 import com.itxiaoer.plugins.mybatis.util.JavaDocUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
@@ -10,7 +11,9 @@ import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,7 +31,6 @@ public class MapperExtendPlugin extends PluginAdapter {
         return true;
     }
 
-
     @Override
     public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         List<Method> methods = interfaze.getMethods();
@@ -45,7 +47,7 @@ public class MapperExtendPlugin extends PluginAdapter {
         interfaze.addAnnotation("@SuppressWarnings(\"ALL\")");
         // domain name
         String domainObjectName = introspectedTable.getFullyQualifiedTable().getDomainObjectName();
-        // key 
+        // key
 
         List<IntrospectedColumn> primaryKeyColumns = introspectedTable.getPrimaryKeyColumns();
 
@@ -78,6 +80,38 @@ public class MapperExtendPlugin extends PluginAdapter {
         return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
     }
 
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        String property = this.properties.getProperty("ignore", "false");
 
+        if (StringUtils.isNotBlank(property) && Boolean.parseBoolean(property)) {
+            XmlElement rootElement = document.getRootElement();
+            rootElement.getElements()
+                       .stream()
+                       .map(e -> (XmlElement) e)
+                       .filter(x -> Objects.equals("insert", x.getName()))
+                       .map(XmlElement::getElements)
+                       .flatMap(List::stream)
+                       .forEach(x -> {
+                           if (x instanceof TextElement) {
+                               TextElement text = (TextElement) x;
+                               String content = text.getContent();
+                               if (content.startsWith("insert into")) {
+                                   String replace = content.replace("insert into", "insert ignore into");
+                                   Field field = null;
+                                   try {
+                                       field = TextElement.class.getDeclaredField("content");
+                                       field.setAccessible(true);
+                                       field.set(text, replace);
+                                   } catch (Exception e) {
+                                       e.printStackTrace();
+                                   }
+                               }
+                           }
+                       });
+        }
+
+        return super.sqlMapDocumentGenerated(document, introspectedTable);
+    }
 }
 
